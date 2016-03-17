34b0623430e2fae2d4c28d6b1eb7135c20818e27
Diego Rodríguez Barreda <barreda_diego@hotmail.com>
2016-03-16 18:27:05 -0600

    practicas

--- prueba de funcionamiento.R
+++ prueba de funcionamiento.R
@@ -58,3 +58,62 @@
             #grafica
                 plot(table(mensajes))
                 
+                
+                
+                
+                
+z <- 5
+k <- 1
+secuencia <- vector("numeric",0)
+while(z>=0 && z<=10){
+    length(secuencia) <- length(secuencia) + 1
+    secuencia[k] <- z
+    moneda <- rbinom(1,1,.5)
+    if (moneda==1){
+        z<- z+1
+        secuencia[k] <- z
+    } else {
+        z<-z-1
+        secuencia[k] <- z
+    }
+    k <- k+1
+    if(k==100000) break
+}
+secuencia
+plot(secuencia,type = "h")
+
+
+mayor10 <- function(x){
+    x[x>10]
+}                
+
+mayor <- function(x,y){
+    x[x>y]
+}
+
+
+
+
+promedioCols <- function(x,na.rm= TRUE){
+    nc <- ncol(x)
+    medias <- vector("numeric", nc)
+    for(i in 1:nc){
+        medias[i] <- mean(x[,i],na.rm = na.rm)
+    }  
+    medias
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
